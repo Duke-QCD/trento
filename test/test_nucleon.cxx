501618438b7c74a791b974e528a7a57ac222f323
Jonah Bernhard <jonah.bernhard@gmail.com>
2015-02-27 11:29:00 -0500

    Set up tests.

---
+++ test/test_nucleon.cxx
@@ -0,0 +1,14 @@
+// TRENTO: Reduced Thickness Event-by-event Nuclear Topology
+// Copyright 2015 Jonah E. Bernhard, J. Scott Moreland
+// MIT License
+
+#include "../src/nucleon.h"
+
+#include "catch.hpp"
+
+using namespace trento;
+
+TEST_CASE( "Nucleons" ) {
+  Nucleon n;
+  CHECK( 1 == 1 );
+}
